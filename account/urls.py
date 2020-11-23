2e7d5a7e9fde8fabec94eecb57d88e3cf5de2af2
vacyny <k@lomati.ru>
2020-11-23 22:28:58 +0300

    Изменение пароля шаблон и форма

--- account/urls.py
+++ account/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', views.dashboard, name='dashboard'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 ]
