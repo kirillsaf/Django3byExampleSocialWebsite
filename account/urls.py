f160600473d63adc87a81fe155a52a9238e1daac
vacyny <k@lomati.ru>
2020-11-24 17:28:24 +0300

    Добавили регистрацию

--- account/urls.py
+++ account/urls.py
@@ -18,4 +18,5 @@ urlpatterns = [
 
     path('', views.dashboard, name='dashboard'),
     path('', include('django.contrib.auth.urls')),
+    path('register/', views.register, name='register'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 from django.contrib.auth.decorators import login_required
 
 
@@ -27,3 +27,19 @@ def user_login(request):
 @login_required
 def dashboard(request):
     return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Создайте новый объект пользователя, но пока не сохраняйте его
+            new_user = user_form.save(commit=False)
+            # Установите выбранный пароль
+            new_user.set_password(user_form.cleaned_data['password'])
+            # Сохранить объект пользователя
+            new_user.save()
+            return render(request, 'account/register_done.html', {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'account/register.html', {'user_form': user_form})
