f8e891a0f991c05978e5a68ea6f9de71e02785de
vacyny <k@lomati.ru>
2020-11-24 18:44:10 +0300

    Добавили редактирование профиля и добавление фото

--- account/urls.py
+++ account/urls.py
@@ -19,4 +19,5 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('', include('django.contrib.auth.urls')),
     path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit'),
 ]
