6dc97b333384080b498e57f92af5e7b1e54d8897
vacyny <k@lomati.ru>
2020-11-30 15:04:07 +0300

    Предотвращение дублирования действий в потоке действий

--- account/views.py
+++ account/views.py
@@ -7,6 +7,7 @@ from django.contrib import messages
 from django.shortcuts import get_object_or_404
 from django.views.decorators.http import require_POST
 from common.decorators import ajax_required
+from actions.utils import create_action
 from .models import Profile, Contact
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 
@@ -47,6 +48,7 @@ def register(request):
             new_user.save()
             # Создание профиля пользователя
             Profile.objects.create(user=new_user)
+            create_action(new_user, 'создал учетную запись')
             return render(request, 'account/register_done.html', {'new_user': new_user})
     else:
         user_form = UserRegistrationForm()
@@ -93,6 +95,7 @@ def user_follow(request):
             user = User.objects.get(id=user_id)
             if action == 'follow':
                 Contact.objects.get_or_create(user_from=request.user, user_to=user)
+                create_action(request.user, 'is following', user)
             else:
                 Contact.objects.filter(user_from=request.user, user_to=user).delete()
             return JsonResponse({'status': 'ok'})
--- images/views.py
+++ images/views.py
@@ -9,6 +9,7 @@ from django.views.decorators.http import require_POST
 from common.decorators import ajax_required
 from django.http import HttpResponse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from actions.utils import create_action
 
 
 @login_required
@@ -23,6 +24,7 @@ def image_create(request):
             # назначить текущего пользователя элементу
             new_item.user = request.user
             new_item.save()
+            create_action(request.user, 'закладка изображения', new_item)
             messages.success(request, 'Изображение успешно добавлено')
             # перенаправить на подробный просмотр нового созданного элемента
             return redirect(new_item.get_absolute_url())
@@ -48,6 +50,7 @@ def image_like(request):
             image = Image.objects.get(id=image_id)
             if action == 'Нравиться':
                 image.users_like.add(request.user)
+                create_action(request.user, 'нравится', image)
             else:
                 image.users_like.remove(request.user)
             return JsonResponse({'status': 'ok'})
