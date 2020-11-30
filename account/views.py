e12263664792988956955244eb54c4b7756a876a
vacyny <k@lomati.ru>
2020-11-30 13:14:32 +0300

    создадим простое представление для follow/unfollow подписки пользователя с помощью AJAX

--- account/views.py
+++ account/views.py
@@ -1,11 +1,13 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.shortcuts import get_object_or_404
-from .models import Profile
+from django.views.decorators.http import require_POST
+from common.decorators import ajax_required
+from .models import Profile, Contact
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 
 
@@ -78,3 +80,22 @@ def user_list(request):
 def user_detail(request, username):
     user = get_object_or_404(User, username=username, is_active=True)
     return render(request, 'account/user/detail.html', {'section': 'people', 'user': user})
+
+
+@ajax_required
+@require_POST
+@login_required
+def user_follow(request):
+    user_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if user_id and action:
+        try:
+            user = User.objects.get(id=user_id)
+            if action == 'follow':
+                Contact.objects.get_or_create(user_from=request.user, user_to=user)
+            else:
+                Contact.objects.filter(user_from=request.user, user_to=user).delete()
+            return JsonResponse({'status': 'ok'})
+        except User.DoesNotExist:
+            return JsonResponse({'status': 'error'})
+    return JsonResponse({'status': 'error'})
